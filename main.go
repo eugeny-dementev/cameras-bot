8c80109d5409f4c83ab965b369493d095989a6b5
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-08-09 22:27:18 +0800

    feat: array of cameras config

--- main.go
+++ main.go
@@ -12,9 +12,16 @@ import (
 	_ "eugeny-dementev.github.io/cameras-bot/ntgcalls"
 )
 
+type CameraConf struct {
+  Id string `json:"id"`
+  User string `json:"user"`
+  Pass string `json:"pass"`
+}
+
 type Config struct {
 	AppId string `json:"app_id"`
   AppHash string `json:"app_hash"`
+  Cameras []CameraConf `json:"cameras"`
 }
 
 func main() {
@@ -22,7 +29,7 @@ func main() {
 	appHash := os.Getenv("APP_HASH")
 
 	myConf := Config{}
-  bytes := []byte(`{ "app_id": "some string id", "app_hash": "some hash string" }`)
+  bytes := []byte(`{ "app_id": "some string id", "app_hash": "some hash string", "cameras": [{ "user": "admin", "pass": "password", "id": "bedroom"}]}`)
 	err := json.Unmarshal(bytes, &myConf)
 	if err != nil {
 		log.Fatal(err)
