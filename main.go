ebe25fc9f0aec902721a944c02162b770684da0f
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-08-09 22:18:47 +0800

    feat: some experiments with Unmarshaling

--- main.go
+++ main.go
@@ -4,15 +4,29 @@ package main
 import "C"
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	_ "eugeny-dementev.github.io/cameras-bot/ntgcalls"
 )
 
+type Config struct {
+	AppId string `json:"app_id"`
+  AppHash string `json:"app_hash"`
+}
+
 func main() {
 	appId := os.Getenv("APP_ID")
 	appHash := os.Getenv("APP_HASH")
 
-	fmt.Println("ENV:", appId, appHash)
+	myConf := Config{}
+  bytes := []byte(`{ "app_id": "some string id", "app_hash": "some hash string" }`)
+	err := json.Unmarshal(bytes, &myConf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("ENV:", appId, appHash, myConf)
 }
