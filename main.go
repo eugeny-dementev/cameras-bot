b57d902c39262a98dd4c2b2bd9879332211c5796
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-08-13 21:30:15 +0800

    feat: setup example command bot

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"time"
 
 	tg "github.com/amarnathcjd/gogram/telegram"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 
 	_ "eugeny-dementev.github.io/cameras-bot/ntgcalls"
 )
@@ -78,8 +83,95 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Get botToken from the environment variable
+	botToken := os.Getenv("BOT_TOKEN")
+	if botToken == "" {
+		panic("TOKEN environment variable is empty")
+	}
 
-	fmt.Println("CONFIG:", conf)
+	// Create bot from environment value.
+	b, err := gotgbot.NewBot(botToken, nil)
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	// Create updater and dispatcher.
+	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
+		// If an error is returned by a handler, log it and continue going.
+		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+			log.Println("an error occurred while handling update:", err.Error())
+			return ext.DispatcherActionNoop
+		},
+		MaxRoutines: ext.DefaultMaxRoutines,
+	})
+	updater := ext.NewUpdater(dispatcher, nil)
+
+	// /start command to introduce the bot
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	// /source command to send the bot source code
+	dispatcher.AddHandler(handlers.NewCommand("source", source))
+
+	// Start receiving updates.
+	err = updater.StartPolling(b, &ext.PollingOpts{
+		DropPendingUpdates: true,
+		GetUpdatesOpts: &gotgbot.GetUpdatesOpts{
+			Timeout: 9,
+			RequestOpts: &gotgbot.RequestOpts{
+				Timeout: time.Second * 10,
+			},
+		},
+	})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+	log.Printf("%s has been started...\n", b.User.Username)
 
 	mtproto.Idle()
+	updater.Idle()
+}
+
+func source(b *gotgbot.Bot, ctx *ext.Context) error {
+	// Sending a file by file handle
+	f, err := os.Open("samples/commandBot/main.go")
+	if err != nil {
+		return fmt.Errorf("failed to open source: %w", err)
+	}
+
+	m, err := b.SendDocument(ctx.EffectiveChat.Id,
+		gotgbot.InputFileByReader("source.go", f),
+		&gotgbot.SendDocumentOpts{
+			Caption: "Here is my source code, by file handle.",
+			ReplyParameters: &gotgbot.ReplyParameters{
+				MessageId: ctx.EffectiveMessage.MessageId,
+			},
+		})
+	if err != nil {
+		return fmt.Errorf("failed to send source: %w", err)
+	}
+
+	// Or sending a file by file ID
+	_, err = b.SendDocument(ctx.EffectiveChat.Id,
+		gotgbot.InputFileByID(m.Document.FileId),
+		&gotgbot.SendDocumentOpts{
+			Caption: "Here is my source code, sent by file id.",
+			ReplyParameters: &gotgbot.ReplyParameters{
+				MessageId: ctx.EffectiveMessage.MessageId,
+			},
+		})
+	if err != nil {
+		return fmt.Errorf("failed to send source: %w", err)
+	}
+
+	return nil
+}
+
+// start introduces the bot.
+func start(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, I'm @%s. I <b>repeat</b> all your messages.", b.User.Username), &gotgbot.SendMessageOpts{
+		ParseMode: "html",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send start message: %w", err)
+	}
+	return nil
 }
