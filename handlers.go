e89833eab1e037b03cb663ff24801b831dd2d762
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-09-03 21:08:42 +0800

    feat: add custom handlers for Start and About commands

---
+++ handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
+
+func StartCmd(c *HandlerContext) error {
+	_, err := c.bot.SendMessage(
+		c.ctx.EffectiveChat.Id,
+		fmt.Sprintf("Hello I'm @%s. I give you access to IP cameras", c.bot.Username),
+		&gotgbot.SendMessageOpts{},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to send start message: %w", err)
+	}
+
+	return nil
+}
+
+func AboutCmd(c *HandlerContext) error {
+	permissions := c.app.config.GetPermissionsFor(c.ctx.EffectiveUser.Id)
+
+	_, err := c.ctx.EffectiveChat.SendMessage(
+		c.bot,
+		fmt.Sprintf("Bot to stream video from IP security cameras\nAvailable cameras: `%v`", permissions.Tags),
+		&gotgbot.SendMessageOpts{
+			DisableNotification: true,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to echo message: %w", err)
+	}
+
+	return nil
+}
